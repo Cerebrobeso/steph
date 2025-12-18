0b51c4c9f711406197ed26f50287d1e2ed14e945
Stefano Silvestro <stefano.silvestro@kerberosweb.com>
2025-12-18 16:18:37 +0100

    feat: translation

--- src/app/shared/services/translate-client.loader.ts
+++ src/app/shared/services/translate-client.loader.ts
@@ -2,8 +2,11 @@ import {TranslateLoader} from '@ngx-translate/core';
 import {makeStateKey, StateKey, TransferState} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 export class TranslateBrowserLoader implements TranslateLoader {
+  env = environment;
+
   constructor(
     private http: HttpClient,
     private transferState: TransferState,
@@ -15,6 +18,9 @@ export class TranslateBrowserLoader implements TranslateLoader {
   getTranslation(lang: string): Observable<any> {
     const key: StateKey<any> = makeStateKey<any>('transfer-translate-' + lang);
     const data = this.transferState.get(key, null);
+    if (!this.env.production) {
+      this.prefix = './public/i18n/';
+    }
 
     if (data) {
       return new Observable((observer) => {
--- src/app/shared/services/translate-server.loader.ts
+++ src/app/shared/services/translate-server.loader.ts
@@ -3,13 +3,20 @@ import { makeStateKey, StateKey, TransferState } from '@angular/core';
 import { Observable } from 'rxjs';
 import * as fs from 'fs';
 import { join } from 'path';
+import {environment} from '../../../environments/environment';
 
 export class TranslateServerLoader implements TranslateLoader {
+  env = environment;
   constructor(private transferState: TransferState) {}
 
   getTranslation(lang: string): Observable<any> {
     return new Observable((observer) => {
-      const path = join(process.cwd(), 'dist/steph/browser/i18n', `${lang}.json`);
+      let prefix = 'dist/steph/browser/i18n';
+      if (!this.env.production) {
+        prefix = './public/i18n';
+      }
+
+      const path = join(process.cwd(), prefix, `${lang}.json`);
       const jsonData = JSON.parse(fs.readFileSync(path, 'utf8'));
       const key: StateKey<any> = makeStateKey('transfer-translate-' + lang);
       this.transferState.set(key, jsonData);
